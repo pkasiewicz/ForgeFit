4834e338f62593f8e39a17b15f92aedf3e7d0cbe
piotrkasiewicz <ptrkasiewicz@gmail.com>
2025-09-07 14:59:11 +0200

    Initialize ForgeFit project with Spring Boot setup and necessary configurations

---
+++ src/main/java/pl/pkasiewicz/forgefit/ForgeFitApplication.java
@@ -0,0 +1,13 @@
+package pl.pkasiewicz.forgefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgeFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForgeFitApplication.class, args);
+    }
+
+}
